458fd0cf43e07380246abba8b65ebbea9561aa4c
Derek Day <derek.andrew.day@gmail.com>
2024-05-16 04:53:31 -0400

    fix her

--- components/Content.jsx
+++ components/Content.jsx
@@ -29,7 +29,7 @@ const Content = () => (
       <h7 className='text-white py-2 px-1 mt-5 mb-5' style={{pointerEvents: 'none', width: "12rem", color:"#f8f9fa", letterSpacing:"0.1rem", borderColor: "#f8f9fa", border: "0.8px solid white", borderRadius: "1rem", textAlignLast: "center", fontWeight: "300" }}>DRAKEHAVEN</h7>
       {/* <h7 className='text-white py-2 px-1 mt-5 mb-5' style={{pointerEvents: 'none', width: "12rem", color:"#f8f9fa", letterSpacing:"0.1rem", borderColor: "#f8f9fa", border: "0.8px solid white", borderRadius: "1rem", textAlignLast: "center", fontWeight: "300" }}>TYE SHEETS</h7> */}
       <h1 className='{`${francisco.src}`} mb-5' style={{color: '#EE3623', fontFamily: 'Francisco', letterSpacing: '0.05rem', fontWeight: '100', fontSize: 'rem', opacity: '0.92'}}>Where the Dragons once wept</h1>
-      <img src='/hero1.png' style={{width: "100%"}} />
+      <img src='/hero.png' style={{width: "100%"}} />
   </div>
   // <div className="next-steps my-5" data-testid="content">
   //   <h2 className="my-5 text-center" data-testid="content-title">
