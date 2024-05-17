15ce73026b3ea7c0d89115055e67ea0cfb1a1009
Derek Day <derek.andrew.day@gmail.com>
2024-05-17 04:21:52 -0400

    header

--- components/Content.jsx
+++ components/Content.jsx
@@ -17,9 +17,11 @@ const Content = () => (
   >
 
       {/* <h6 className='text-white px-1 mt-5 mb-5' style={{paddingTop: '0.5rem !important', paddingBottom: '0.55rem !important', pointerEvents: 'none', width: "12rem", color:"#f8f9fa", letterSpacing:"0.1rem", borderColor: "#f8f9fa", border: "0.8px solid white", borderRadius: "1rem", textAlignLast: "center", fontWeight: "300" }}>DRAKEHAVEN</h6> */}
-      <h2 className='title mb-4' style={{fontFamily:'House', letterSpacing:'0.3rem', fontSize: '4rem' }}>Drakehaven</h2>
+      <h2 className='title mt-2 mb-5' style={{fontFamily:'House', letterSpacing:'0.3rem', fontSize: '6rem' }}>Drakehaven</h2>
       {/* <h1 className='{`${francisco.src}`} mb-5 mx-5' style={{color: '#f3482a', fontFamily: 'Francisco', fontKerning: 'none', letterSpacing: '0.05rem', fontWeight: '100', fontSize: '2rem'}}>Welcome to the century city - a land free from all dragons.</h1> */}
-      <img className='hero-image' src='/hero.png' style={{width: "100%", filter: "saturate(0.85)"}} />
+      {/* <img className='hero-image' src='/hero.png' style={{width: "100%", filter: "saturate(0.85)"}} /> */}
+      {/* <img className='hero-image' src='/herocomic.png' style={{width: "100%", filter: "saturate(0.85)"}} /> */}
+      <img className='hero-image' src='/heroold.png' style={{width: "100%", filter: "saturate(0.85)"}} />
   </div>
   // <div className="next-steps my-5" data-testid="content">
   //   <h2 className="my-5 text-center" data-testid="content-title">
