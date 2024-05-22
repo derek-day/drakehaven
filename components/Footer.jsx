2a03aee53508d4ee5dccc4337e054ce584c3132e
Derek Day <derek.andrew.day@gmail.com>
2024-05-21 22:48:18 -0400

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => (
   // <footer className="p-3 text-center" data-testid="footer">
-    <div className="p-3 text-right">
-      <a className="footer_img" target="_blank" rel="noopener noreferrer" href='https://www.youtube.com/@fudgedroll7145' ><img src='/fudged.png' style={{mixBlendMode:'lighten', width:'5rem'}} /></a>
+    <div className="p-3 text-right mt-2">
+      <a className="footer_img" target="_blank" rel="noopener noreferrer" href='https://www.youtube.com/@fudgedroll7145' ><img src='/fudged.png' /></a>
     </div>
   // </footer>
 );
