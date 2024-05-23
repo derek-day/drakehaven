fdab211a2739eb9fd64d3bd7fcb748751c99e607
Derek Day <derek.andrew.day@gmail.com>
2024-05-23 05:05:12 -0400

    g

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => (
   // <footer className="p-3 text-center" data-testid="footer">
-    <div className="p-3 text-right mt-2">
+    <div className="p-3 text-right mt-4">
       <a className="footer_img" target="_blank" rel="noopener noreferrer" href='https://www.youtube.com/@fudgedroll7145' ><img src='/fudged.png' /></a>
     </div>
   // </footer>
